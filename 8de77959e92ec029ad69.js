771328f865cd0199f9c575e3175fa89f3e83df90
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2025-07-13 13:40:45 +0530

    Deployment commit

--- 8de77959e92ec029ad69.js
+++
@@ -1,1 +0,0 @@
-"use strict";import{LinkedList}from"./modules/linked-list";window.LinkedList=LinkedList;const list=new LinkedList;list.append("dog"),list.append("cat"),list.append("parrot"),list.append("hamster"),list.append("snake"),list.append("turtle"),console.log(list.toString());
---
+++ da40252a2f754d96976c.js
@@ -0,0 +1,1 @@
+"use strict";import{LinkedList}from"./modules/linked-list";import{addInfoToConsole}from"./modules/console-info";window.LinkedList=LinkedList,addInfoToConsole();
